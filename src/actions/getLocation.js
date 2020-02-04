e8b6c1b12f5861fca6648c20cbe109a4413a17dd
John Verdone <jmverdone@gmail.com>
2020-02-04 00:10:58 -0500

    updated envs

--- src/actions/getLocation.js
+++ src/actions/getLocation.js
@@ -5,7 +5,7 @@ const fetchLocationsData = () => {
     dispatch({ type: 'LOADING_LOCATIONS' });
 
     return axios
-      .get(`http://${process.env.REACT_APP_ROOT}/locations`)
+      .get(`${process.env.REACT_APP_ROOT}/locations`)
       .then(res => {
         return res.data;
       })
